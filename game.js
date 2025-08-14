eb7f000c42de7fd8eb653dcf431c3122aa5660d0
Rui Machado <networkrmxx@gmail.com>
2025-08-14 15:46:56 +0100

    First Commit

---
+++ game.js
@@ -0,0 +1,101 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const gameOver = document.getElementById('gameOver');
+const restartBtn = document.getElementById('restartBtn');
+const permissionScreen = document.getElementById('permissionScreen');
+const permissionBtn = document.getElementById('permissionBtn');
+
+
+let ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  radius: 20,
+  color: 'red',
+  speedX: 0,
+  speedY: 0
+};
+
+if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+  permissionBtn.addEventListener('click', () => {
+    DeviceOrientationEvent.requestPermission()
+        .then(permissionState => {
+          if (permissionState === 'granted') {
+            permissionScreen.style.display = 'none';
+            window.addEventListener('deviceorientation', handleOrientation);
+            gameLoop();
+          } else {
+              alert('Permission to use gyroscope denied.');
+          }
+        })
+        .catch(console.error);
+  });
+} else {
+  permissionScreen.style.display = 'none';
+  window.addEventListener('deviceorientation', handleOrientation);
+  gameLoop();
+}
+
+function handleOrientation() {
+  const tiltX = event.gamma;
+
+  const tiltY =  event.beta;
+
+  const sensitivity = 0.5;
+
+  ball.speedX = tiltX * sensitivity;
+  ball.speedY = tiltY * sensitivity;
+}
+
+let isGameOver = false;
+
+function gameLoop() {
+  if (isGameOver){
+    return;
+  }
+
+  update();
+
+  render();
+
+  requestAnimationFrame();
+}
+
+function update() {
+  ball.x += ball.speedX;
+  ball.y += ball.speedY;
+
+  if (ball.x - ball.radius < 0 || ball.x + ball.radius > canvas.width || 
+    ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height) {
+
+    isGameOver = true;
+    gameOver.classList.remove('hidden');
+    }
+}
+
+function render() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
+  ctx.fillStyle = ball.color;
+  ctx.fill();
+  ctx.closePath();
+}
+
+restartBtn.addEventListener('click', () => {
+  isGameOver = false;
+  gameOver.classList.add('hidden');
+  //Reset Ball Pos and speed
+  ball.x = canvas.width / 2;
+  ball.y = canvas.height / 2;
+  ball.speedX = 0;
+  ball.speedY = 0;
+
+  gameLoop();
+});
+
+gameLoop();
+
+
+
+
